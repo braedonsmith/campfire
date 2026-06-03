73d8f62b62deb7040e500b5c486a9703eb348011
Braedon Smith <braedon.smith@kswg.cap.gov>
2026-06-03 18:13:44 -0500

    Added profile information display

--- resources/js/pages/settings/Profile.vue
+++ resources/js/pages/settings/Profile.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
-import { Form, Head, Link, usePage } from '@inertiajs/vue3';
+import { Head, usePage } from '@inertiajs/vue3';
 import { computed } from 'vue';
-import ProfileController from '@/actions/App/Http/Controllers/Settings/ProfileController';
-import DeleteUser from '@/components/DeleteUser.vue';
 import Heading from '@/components/Heading.vue';
-import InputError from '@/components/InputError.vue';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { Table, TableBody, TableCaption, TableCell, TableRow } from '@/components/ui/table';
 import { edit } from '@/routes/profile';
-import { send } from '@/routes/verification';
 
 type Props = {
     mustVerifyEmail: boolean;
@@ -42,70 +36,33 @@ const user = computed(() => page.props.auth.user);
         <Heading
             variant="small"
             title="Profile information"
-            description="Update your name and email address"
         />
 
-        <Form
-            v-bind="ProfileController.update.form()"
-            class="space-y-6"
-            v-slot="{ errors, processing }"
-        >
-            <div class="grid gap-2">
-                <Label for="name">Name</Label>
-                <Input
-                    id="name"
-                    class="mt-1 block w-full"
-                    name="name"
-                    :default-value="user.name"
-                    required
-                    autocomplete="name"
-                    placeholder="Full name"
-                />
-                <InputError class="mt-2" :message="errors.name" />
-            </div>
-
-            <div class="grid gap-2">
-                <Label for="email">Email address</Label>
-                <Input
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    name="email"
-                    :default-value="user.email"
-                    required
-                    autocomplete="username"
-                    placeholder="Email address"
-                />
-                <InputError class="mt-2" :message="errors.email" />
-            </div>
-
-            <div v-if="mustVerifyEmail && !user.email_verified_at">
-                <p class="-mt-4 text-sm text-muted-foreground">
-                    Your email address is unverified.
-                    <Link
-                        :href="send()"
-                        as="button"
-                        class="text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500"
-                    >
-                        Click here to resend the verification email.
-                    </Link>
-                </p>
-
-                <div
-                    v-if="status === 'verification-link-sent'"
-                    class="mt-2 text-sm font-medium text-green-600"
-                >
-                    A new verification link has been sent to your email address.
-                </div>
-            </div>
-
-            <div class="flex items-center gap-4">
-                <Button :disabled="processing" data-test="update-profile-button"
-                    >Save</Button
-                >
-            </div>
-        </Form>
+        <Table>
+            <TableCaption>This information is controlled by eServices. If any of this information appears wrong, contact the Administrative Office for support.</TableCaption>
+            <TableBody>
+                <TableRow>
+                    <TableCell>CAPID</TableCell>
+                    <TableCell>{{ user.capid.toString() }}</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>{{ user.first_name + ' ' + user.last_name }}</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell>Rank</TableCell>
+                    <TableCell>{{ user.rank }}</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell>Email Address</TableCell>
+                    <TableCell>{{ user.email }}</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell>Home Unit</TableCell>
+                    <TableCell>{{ user.home_unit }}</TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
     </div>
-
-    <DeleteUser />
+    <p>Last updated: {{ new Date(user.updated_at).toString() }}</p>
 </template>
